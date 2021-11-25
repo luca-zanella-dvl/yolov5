d320c8447f4d113525489ccbec640e1e1780967d
luca-zanella-dvl <lzanella@fbk.eu>
2021-11-25 14:37:45 +0100

    Fix import modules

--- models/yolo.py
+++ models/yolo.py
@@ -311,7 +311,7 @@ def parse_model(d, ch):  # model_dict, input_channels(3)
             c2 = ch[f] * args[0] ** 2
         elif m is Expand:
             c2 = ch[f] // args[0] ** 2
-        elif m is DiscriminatorConv:
+        elif adv and m is DiscriminatorConv:
             args.insert(0, ch[f])
         else:
             c2 = ch[f]
