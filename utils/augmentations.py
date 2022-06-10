428c0d6bdf18eb12933ac54b4924c99b1dd15df9
luca-zanella-dvl <lzanella@fbk.eu>
2022-06-10 13:59:59 +0200

    Comment out TPH-YOLOv5 augmentations

--- utils/augmentations.py
+++ utils/augmentations.py
@@ -21,6 +21,7 @@ class Albumentations:
             import albumentations as A
             check_version(A.__version__, '1.0.3', hard=True)  # version requirement
 
+            # YOLOv5
             T = [
                 A.Blur(p=0.01),
                 A.MedianBlur(p=0.3),
@@ -30,14 +31,14 @@ class Albumentations:
                 A.RandomGamma(p=0.0),
                 A.ImageCompression(quality_lower=75, p=0.0)]  # transforms
             # TPH-YOLOv5
-            T = [
-                A.Blur(p=0.01),
-                A.MedianBlur(p=0.3),
-                A.ToGray(p=0.01),
-                A.CLAHE(p=0.3),
-                A.RandomBrightnessContrast(p=0.3),
-                A.RandomGamma(p=0.0),
-                A.ImageCompression(quality_lower=75, p=0.0)]  # transforms
+            # T = [
+            #     A.Blur(p=0.01),
+            #     A.MedianBlur(p=0.3),
+            #     A.ToGray(p=0.01),
+            #     A.CLAHE(p=0.3),
+            #     A.RandomBrightnessContrast(p=0.3),
+            #     A.RandomGamma(p=0.0),
+            #     A.ImageCompression(quality_lower=75, p=0.0)]  # transforms
             self.transform = A.Compose(T, bbox_params=A.BboxParams(format='yolo', label_fields=['class_labels']))
 
             LOGGER.info(colorstr('albumentations: ') + ', '.join(f'{x}' for x in self.transform.transforms if x.p))
